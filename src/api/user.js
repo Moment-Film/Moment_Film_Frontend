c3907e202f6e1d7b68157bde296e4402da7949eb
abxl1 <etho_818@naver.com>
2023-08-02 01:26:09 +0900

    [ feat ] 회원가입 유효성 검사, 사용자 검색 api, 마이페이지 컴포넌트 임시 구현

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import ourAxios from './ourAxios';
 
 export const register = async ({username, password, email, phone}) => {
     try {
-        const res = await ourAxios.post('/api/user/signup', {
+        const res = await ourAxios.post('/user', {
             username,
             email,
             password,
