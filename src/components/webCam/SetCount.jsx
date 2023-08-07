3fdebb27ff954c4ab4750d670a09685551578f9d
2seo <yuj07149@gmail.com>
2023-08-07 21:48:36 +0900

    [style] 사진 선택 페이지 스타일 (1)

--- src/components/webCam/SetCount.jsx
+++ src/components/webCam/SetCount.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { DndProvider, useDrop, useDrag } from 'react-dnd';
 import {TouchBackend} from 'react-dnd-touch-backend';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useNavigate } from 'react-router-dom';
+import GridNav from '../frameSelectPage/GridNav'
+import right_arrow from '../assets/images/right_arrow.png'
+import {
+  CapturedPhotos,
+  MoveButton,
+  SlilderWrap,
+  ImageSlider,
+} from './style'
 
 
 const ImagePiece = ({ imageSrc, onDrop, isPlaced }) => {
@@ -18,19 +26,21 @@ const ImagePiece = ({ imageSrc, onDrop, isPlaced }) => {
     <img
       ref={dragRef}
       src={imageSrc}
-      alt="Puzzle Piece"
+      alt="CapturedImage"
       style={{
         cursor: "pointer",
-        width: '100px',
-        margin: "10px",
+        width: '168px',
         opacity: isDragging ? 0.5 : 1,
         display: isPlaced ? 'none' : 'block',
+        borderRight: "3px solid black",
+        borderLeft: "3px solid black",
+        boxSizing: "border-box"
       }}
     />
   );
 };
 
-const PuzzleBoard = ({ onDrop, imageSrc }) => {
+const GridInner = ({ onDrop, imageSrc }) => {
   const [{ canDrop, isOver }, dropRef] = useDrop({
     accept: 'imagePiece',
     drop: (item, monitor) => {
@@ -46,8 +56,8 @@ const PuzzleBoard = ({ onDrop, imageSrc }) => {
     <div
       ref={dropRef}
       style={{
-        width: '384px',
-        height: '288px',
+        width: '123px',
+        height: '162px',
         background: 'white',
         margin: '10px',
         display: 'flex',
@@ -61,9 +71,10 @@ const PuzzleBoard = ({ onDrop, imageSrc }) => {
 };
 
 const SetCount = () => {
+  const slideRef = useRef(null);
   const navigate = useNavigate();
   const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-  const [images, setImages] = useState([
+  const [capturedImages, setCapturedImages] = useState([
     { id: 1, src: localStorage.getItem(`image0`) },
     { id: 2, src: localStorage.getItem(`image1`)  },
     { id: 3, src: localStorage.getItem(`image2`)  },
@@ -73,6 +84,32 @@ const SetCount = () => {
     { id: 7, src: localStorage.getItem(`image6`)  },
     { id: 8, src: localStorage.getItem(`image7`)  },
   ]);
+  const [imgCnt, setImgCnt] = useState(capturedImages.length);
+  const [currentImgOrder, setCurrentImgOrder] = useState(0);
+
+  const moveToNextSlide = () => {
+    if (currentImgOrder === 1) return;
+    setCurrentImgOrder(currentImgOrder + 1);
+  };
+
+  const moveToPrevSlide = () => {
+    if (currentImgOrder === 0) return;
+    setCurrentImgOrder(currentImgOrder - 1);
+  };
+
+  useEffect(() => {
+    MoveSlider();
+  }, [currentImgOrder, capturedImages]);
+
+  const MoveSlider = () => {
+    if (slideRef.current !== null) { //즉시할당이 안될수있어서 그냥 옵셔널체이닝 느낌
+      slideRef.current.style.transition = "all 0.5s ease-in-out"; //부드럽게 이동 
+      const size = imgCnt*173+15 // 내부슬라이더 요소의 가로길이 얻기 
+      const element = document.querySelector(SlilderWrap);
+      const slideWidth = size-element.offsetWidth;
+      slideRef.current.style.transform = `translateX(-${(slideWidth) * currentImgOrder}px)`; //얻은 가로길이*페이지 로 x축 이동 
+    }
+  }
 
   const [boardImages, setBoardImages] = useState(Array(4).fill(null));
 
@@ -84,29 +121,36 @@ const SetCount = () => {
 
   return (
     <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
-      <div style={{display:"flex", width:"100%", flexDirection: "column", alignItems:" center", marginTop: "100px"}}>
-      <div>
-        <div style={{ display: 'flex', width:"830px", justifyContent: "center",flexWrap:"wrap", backgroundColor:"gray", padding: "50px 0 70px 0"}}>
+      <div style={{display:"flex", width:"1170px", flexDirection: "column", alignItems:" center", margin:"0 auto", backgroundColor: "white", overflow: "hidden"}}>
+        <GridNav data='photoSelect'/>
+      <div style={{width: "1170px", height: "620px", backgroundColor: '#fbfbfb', borderBottom: '1px solid var(--lightGray)'}}>
+        <div style={{ display: 'flex', width: '300px', height:"447px", justifyContent: "center",flexWrap:"wrap", backgroundColor:"gray", padding: "50px 0 70px 0"}}>
           {boardImages.map((imageSrc, index) => (
-            <PuzzleBoard
+            <GridInner
               key={index}
               onDrop={(imgSrc) => handleDrop(imgSrc, index)}
-              imageSrc={imageSrc} />
+              imageSrc={imageSrc}></GridInner>
           ))}
         </div>
       </div>
       <div style={{textAlign:"center"}}>
-        <h4>이미지를 프레임에 드래그 하세요.</h4>
-        <div style={{ display: 'flex' }}>
-          {images.map((image) => (
-            <ImagePiece
-              key={image.id}
-              imageSrc={image.src}
-              onDrop={handleDrop}
-              isPlaced={boardImages.includes(image.src)}
-            />
-          ))}
-        </div>
+      {capturedImages.length > 0 && (
+        <CapturedPhotos>
+          {capturedImages.length>5 && <MoveButton $hide={currentImgOrder===0} onClick={moveToPrevSlide}><img src={right_arrow} style={{ transform: "scale(-1)" }} /></MoveButton>}
+          <SlilderWrap>{/* 전체 슬라이더 영역 범위 밖으로 넘어가면 안보여줄거임*/}
+          <ImageSlider ref={slideRef}> {/* 내부 슬라이더 영역 */}
+            {capturedImages.map((image, index) => (
+              <ImagePiece
+                key={image.id}
+                imageSrc={image.src}
+                onDrop={handleDrop}
+                isPlaced={boardImages.includes(image.src)} />
+            ))}
+          </ImageSlider>
+          </SlilderWrap>
+          {capturedImages.length>5 && <MoveButton $hide={currentImgOrder===1} onClick={moveToNextSlide}><img src={right_arrow} /></MoveButton>}
+        </CapturedPhotos>
+      )}
       </div>
       </div>
       <button onClick={()=>navigate(`../camera/capture/filter`)}>다음으로</button>
--- src/pages/CameraOptionPage.jsx
+++ src/pages/CameraOptionPage.jsx
@@ -3,9 +3,9 @@ import SetCount from "../components/webCam/SetCount";
 
 function CameraOptionPage() {
   return (
-    <>
+    <div style={{width: '100%', backgroundColor:'var(--whiteGray)'}}>
       <SetCount />
-    </>
+    </div>
   )
 }
 export default CameraOptionPage;
