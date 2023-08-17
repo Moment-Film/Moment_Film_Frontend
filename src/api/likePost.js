286239bd606b7dffc15ded4434910c4bbbb0401b
Johyuncheol <51200912+Johyuncheol@users.noreply.github.com>
2023-08-17 21:06:49 +0900

    [feat]: 이미지 꾸미기(그림 및 스티커) 사용 및 게시글 등록
    이미지 꾸미기(그림 및 스티커) 사용 및 게시글 등록

--- src/api/likePost.js
+++ src/api/likePost.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-export const likePost = async ( postId ,ACToken ,REToken ) => {
+export const likePost = async ( {postId ,accessToken ,refreshToken }) => {
     try {
+        console.log(postId)
+        console.log(accessToken)
+
+        console.log(refreshToken)
+
         const res = await axios.post(`/api/post/${postId}/likes`,null,
             {
                 headers: {
-                    accessToken: ACToken,
-                    refreshToken: REToken
+                    accessToken: accessToken,
+                    refreshToken: refreshToken
                 },
             }
         );
         console.log(res);
-        return res.data;
+        return res.data.data;
 
     } catch (error) {
         console.error("likePost api error", error);
--- src/components/detailPage/DetailContent.jsx
+++ src/components/detailPage/DetailContent.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components"
 
 import KakaoShareBtn from "../common/component/KakaoShareBtn"
@@ -7,90 +7,107 @@ import * as S from "../common/styles/StyledSpan"
 import { FollowAPI } from "../../api/snsUser"
 import { likePost } from "../../api/likePost"
 import { getPostDetail, deletePost } from "../../api/post"
-import { useParams,useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-
-const DetailContent = ({data}) => {
+import { useMutation, useQueryClient } from 'react-query';
+const DetailContent = ({ data }) => {
   //필요한 변수 선언
   const param = useParams();
   const location = useLocation();
   const path = location.pathname
+  const queryClient = useQueryClient();
 
+  console.log(data);
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [selectFrame, setSelectFrame] = useState(false);
   const [selectFilter, setSelectFilter] = useState(false);
-  
+
 
   //액세스 토큰 리프레시 토큰 가져오는 부분 
   const [cookie, setCookie] = useCookies(['refresh']);
   const accessToken = useSelector((state) => state.AccessToken.accessToken);
   const refreshToken = cookie.refresh
 
-// 기능 함수들 
-  const postLikeHandler = () => {
-    likePost(param.id, accessToken, refreshToken)
-  }
+  // 기능 함수들 
 
   const FollowHandler = () => {
     FollowAPI(data.userId, accessToken, refreshToken);
   }
 
-  const deleteHandler = async() => {
-    await deletePost(data.id,accessToken, refreshToken)
+  const deleteHandler = async () => {
+    await deletePost(data.id, accessToken, refreshToken)
     navigate(-1);
   }
 
-    return (
-        <div>
-            <DetailHeader>
-                <S.StyledBoldSpan28>Detail Page</S.StyledBoldSpan28>
-            </DetailHeader>
-            <DetailContents>
-                <FrameSection>
-                    <img src={data.image} alt="" />
-                </FrameSection>
+  const getDetailMutation = useMutation(likePost, {
+    onSuccess: (response) => {
+      console.log(response);
+      {
+        queryClient.invalidateQueries(`Detail${param.id}`);
+      }
+    },
+    onError: (error) => {
+      alert("에러");
+    },
+  });
 
-                <DetailPost>
-                    <ViewCount><S.StyledSpan14>view +{data.viewCount}</S.StyledSpan14></ViewCount>
-                    <S.StyledSpan14>작성날짜 {data.createdAt} </S.StyledSpan14>
-                    <S.StyledBoldSpan24>{data.title}</S.StyledBoldSpan24>
-                    <S.StyledSpan18>● {data.username} 님</S.StyledSpan18>
+  // api 동작이 들어있는 함수
+  const postLikeHandler = () => {
+    const postId = param.id;
+    getDetailMutation.mutate({ postId, accessToken, refreshToken });
+  };
 
-                    <Detail>
-                        <S.StyledSpan16>{data.contents}</S.StyledSpan16>
-                    </Detail>
+  return (
+    <div>
+      <DetailHeader>
+        <S.StyledBoldSpan28>Detail Page</S.StyledBoldSpan28>
+      </DetailHeader>
+      <DetailContents>
+        <FrameSection>
+          <img src={data.image} alt="" />
+        </FrameSection>
 
-                    <OptionSection>
-                        <CheckBox $bg={selectFrame} onMouseDown={() => setSelectFrame(!selectFrame)}>
-                            <span >프레임 사용하기</span>
-                            <input type="checkbox" value={selectFrame} checked={selectFrame} />
-                        </CheckBox>
-                        <CheckBox $bg={selectFilter} onMouseDown={() => setSelectFilter(!selectFilter)}>
-                            <span >필터 사용하기</span>
-                            <input type="checkbox" value={selectFilter} checked={selectFilter} />
-                        </CheckBox>
-                        <button>사용해보기</button>
-                    </OptionSection>
+        <DetailPost>
+          <ViewCount><S.StyledSpan14>view +{data.viewCount}</S.StyledSpan14></ViewCount>
+          <S.StyledSpan14>작성날짜 {data.createdAt} </S.StyledSpan14>
+          <S.StyledBoldSpan24>{data.title}</S.StyledBoldSpan24>
+          <S.StyledSpan18>● {data.username} 님</S.StyledSpan18>
 
-                    <PostAction>
-                        <Action>
-                            <S.StyledSpan14>좋아요 수</S.StyledSpan14>
-                            <S.StyledSpan14>{data.likeCount}개</S.StyledSpan14>
-                            <S.StyledSpan14 onClick={postLikeHandler}>하트</S.StyledSpan14>
-                            <KakaoShareBtn path={path} data={data} ></KakaoShareBtn>
-                            <UrlShare></UrlShare>
-                            <button onClick={FollowHandler}>팔로우</button>
-                            <button onClick={deleteHandler}>게시글 삭제</button>
-                        </Action>
-                    </PostAction>
+          <Detail>
+            <S.StyledSpan16>{data.contents}</S.StyledSpan16>
+          </Detail>
 
-                </DetailPost>
-            </DetailContents>
-        </div>
-    );
+          <OptionSection>
+            <CheckBox $bg={selectFrame} onMouseDown={() => setSelectFrame(!selectFrame)}>
+              <span >프레임 사용하기</span>
+              <input type="checkbox" value={selectFrame} checked={selectFrame} />
+            </CheckBox>
+            <CheckBox $bg={selectFilter} onMouseDown={() => setSelectFilter(!selectFilter)}>
+              <span >필터 사용하기</span>
+              <input type="checkbox" value={selectFilter} checked={selectFilter} />
+            </CheckBox>
+            <button>사용해보기</button>
+          </OptionSection>
+
+          <PostAction>
+            <Action>
+              <S.StyledSpan14>좋아요 수</S.StyledSpan14>
+              <S.StyledSpan14>{data.likeCount}개</S.StyledSpan14>
+              <S.StyledSpan14 onClick={postLikeHandler}>하트</S.StyledSpan14>
+              <KakaoShareBtn path={path} data={data} ></KakaoShareBtn>
+              <UrlShare></UrlShare>
+              <button onClick={FollowHandler}>팔로우</button>
+              <button onClick={deleteHandler}>게시글 삭제</button>
+            </Action>
+          </PostAction>
+
+        </DetailPost>
+      </DetailContents>
+    </div>
+  );
 };
 
 export default DetailContent;
