d988b21900865e1cf162ecd722caec8a379dc058
abxl1 <etho_818@naver.com>
2023-08-18 16:19:52 +0900

    [ style ] 마이페이지 UI 수정

--- src/components/myPage/MyPageNav.jsx
+++ src/components/myPage/MyPageNav.jsx
@@ -1,44 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const MyPageNav = () => {
-    return (
-        <NavSection>
-            <NavWrap>
-            <hr/>
-            <StyledLink>등록한 디자인</StyledLink>
-            <hr/>
-            <StyledLink>관심있는 디자인</StyledLink>
-            <hr/>
-            </NavWrap>
-        </NavSection>
-    );
+  return (
+    <NavSection>
+      <NavWrap>
+        <hr />
+        <StyledLink>등록한 디자인</StyledLink>
+        <hr />
+        <StyledLink>관심있는 디자인</StyledLink>
+        <hr />
+      </NavWrap>
+    </NavSection>
+  );
 };
 
 export default MyPageNav;
 
 const NavSection = styled.section`
-    display:flex;
-    width:60%;
-    justify-content:center;
-    gap:20px;
-    border-bottom: 1px solid black;
-
-`
+  display: flex;
+  width: 60%;
+  justify-content: center;
+  gap: 20px;
+  border-bottom: 1px solid var(--black);
+`;
 
 const NavWrap = styled.section`
-    display:flex;
-    width:40%;
-    align-items:center;
-    hr{
-        height:10px;
-    }
-
-
-`
-const StyledLink=styled(Link)`
-    text-decoration:none;
-    height: 15px;
-    
-`
+  display: flex;
+  width: 40%;
+  align-items: center;
+  hr {
+    height: 10px;
+    border: 1px solid var(--lightGray);
+  }
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  height: 15px;
+  border-bottom: 2px solid var(--green5);
+  padding: 17px;
+`;
