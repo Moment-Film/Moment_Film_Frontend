11cbc87a5e34a38940923ebfa66e45dd2b844b8e
2seo <yuj07149@gmail.com>
2023-08-25 19:53:18 +0900

    [fix] 로그아웃 시 리덕스 userInfo 삭제, 로그인 상태로 회원가입 페이지 접근x

--- src/components/common/component/LogoutBtn.jsx
+++ src/components/common/component/LogoutBtn.jsx
@@ -5,12 +5,12 @@ import { useNavigate } from "react-router";
 import { styled } from "styled-components";
 import useToken from "../../../hooks/useToken";
 import useAuthAPI from "../../../api/nonToken/auth";
+import { SetUserInfo } from "../../../redux/modules/User";
 
 const LogoutBtn = () => {
   const { LogOutAPI } = useAuthAPI();
 
-  const { getAccess, getRefresh } = useToken();
-
+  const { getAccess, getRefresh, saveUserInfo } = useToken();
   const refreshToken = getRefresh();
   const accessToken = getAccess();
 
@@ -21,6 +21,7 @@ const LogoutBtn = () => {
     const res = await LogOutAPI(accessToken, refreshToken);
 
     await dispatch(SetAccessToken(null));
+    await dispatch(SetUserInfo(null));
     console.log(accessToken);
     navigate("/");
   };
