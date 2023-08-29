ad91c69baba2a293f55f09d48c203b7164d7968e
abxl1 <etho_818@naver.com>
2023-08-29 19:35:45 +0900

    [ feat ] 검색 결과 페이지네이션 구현 중 / 결과 없음 페이지 UI 구현

--- src/components/common/component/PageNation.jsx
+++ src/components/common/component/PageNation.jsx
@@ -1,77 +1,82 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import ContentBox from './ContentBox';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import ContentBox from "./ContentBox";
+import SearchUserData from "../../searchResultPage/SearchUserData";
 
-const PaginationComponent = ({ data = [] ,ItemNums }) => {
+const PaginationComponent = ({ data, ItemNums, category }) => {
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    
-    if (!Array.isArray(data) || data.length === 0) {
-        return null;  // 또는 유효하지 않은 데이터에 대한 처리나 기본 메시지를 반환할 수 있습니다.
-    }
+  if (!Array.isArray(data) || data.length === 0) {
+    return null; // 또는 유효하지 않은 데이터에 대한 처리나 기본 메시지를 반환할 수 있습니다.
+  }
 
-    // 현재 페이지에 보여줄 데이터 계산
-    const LastIndex = currentPage * ItemNums;
-    const FirstIndex = LastIndex - ItemNums;
-    const currentItems = data.slice(FirstIndex, LastIndex);
+  // 현재 페이지에 보여줄 데이터 계산
+  const LastIndex = currentPage * ItemNums;
+  const FirstIndex = LastIndex - ItemNums;
+  const currentItems = data.slice(FirstIndex, LastIndex);
 
-    // 페이지 변경 이벤트 핸들러
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
+  // 페이지 변경 이벤트 핸들러
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
+	console.log(data)
 
-    return (
-        <PageNationSection>
-            <ContentsSection>
-                <ContentBox data={currentItems}></ContentBox>
-            </ContentsSection>
+  return (
+    <PageNationSection>
+      {category === "result" 
+      ? (
+        <SearchUserData data={currentItems} />
+      ) 
+      : (
+        <ContentsSection>
+          <ContentBox data={currentItems}></ContentBox>
+        </ContentsSection>
+      )}
 
-
-            {/* 페이지네이션 버튼 */}
-            {/*  Array.from({length: 페이지 수}, (value(안씀), index) => {});  */}
-            <PageBtnSection> 
-                {Array.from({ length: Math.ceil(data.length / ItemNums) }, (_, index) => {
-                    return (
-                    <button key={index} onClick={() => handlePageChange(index + 1)}>
-                        {index + 1}
-                    </button>
-                    );
-                })}
-            </PageBtnSection>
-        </PageNationSection>
-    );
+      {/* 페이지네이션 버튼 */}
+      {/*  Array.from({length: 페이지 수}, (value(안씀), index) => {});  */}
+      <PageBtnSection>
+        {Array.from(
+          { length: Math.ceil(data.length / ItemNums) },
+          (_, index) => {
+            return (
+              <button key={index} onClick={() => handlePageChange(index + 1)}>
+                {index + 1}
+              </button>
+            );
+          }
+        )}
+      </PageBtnSection>
+    </PageNationSection>
+  );
 };
 
 export default PaginationComponent;
 
-
 const PageNationSection = styled.div`
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    width:90%;
-    max-width:1170px;
-    flex-direction:column;
-
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  max-width: 1170px;
+  flex-direction: column;
+`;
 
 const ContentsSection = styled.section`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    //flex-wrap:wrap;
-    gap:2%;
-`
-
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  //flex-wrap:wrap;
+  gap: 2%;
+`;
 
 const PageBtnSection = styled.div`
-    padding:10px;
+  padding: 10px;
 
-    button{
-        width:50px;
-        height:50px;
-    }
-`
+  button {
+    width: 50px;
+    height: 50px;
+  }
+`;
