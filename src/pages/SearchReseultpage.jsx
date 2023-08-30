12787796982fc3316902ce3ff8170781550f7d9d
abxl1 <etho_818@naver.com>
2023-08-30 16:32:08 +0900

    [ style ] 미사용 컴포넌트 삭제 / 수정

--- src/pages/SearchReseultpage.jsx
+++ src/pages/SearchReseultpage.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-// import InfiniteScroll from "../components/common/component/InfinityScroll";
 import SearchReseult from "../components/searchResultPage/SearchResult";
 
 function SearchReseultpage() {
-// 어떤 내용에 무한스크롤을 적용할지 props를 내려 줄 것
+
   return (
-    <>
-      <SearchReseult />
-      {/* <InfiniteScroll />  */}
-    </>
+    <SearchReseult />
   );
 }
 
