1e4fc4978abd0a805ea9392b3b38ff1134a3a343
abxl1 <etho_818@naver.com>
2023-09-01 04:37:57 +0900

    [ style ] footer UI / navigate + link 구현

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,77 @@
-import styled from "styled-components"
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import FooterLogo from "../components/assets/images/FooterLogo.svg";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
-    <FooterBox>Footer</FooterBox>
-  )
+    <FooterBox>
+      <ContentsWrap>
+        <section className="align">
+          <div>CONTACT</div>
+          <div><span /></div>
+          <div>Discord</div>
+          <div>E-mail</div>
+          <div>Instagram</div>
+        </section>
+
+        <FooterTitle onClick={() => navigate(`/`)}>
+          <img src={FooterLogo} alt="" />
+        </FooterTitle>
+
+        <section className="align">
+          <div>@GitHub</div>
+          <div>Team. Broccoli 🥦</div>
+        </section>
+      </ContentsWrap>
+    </FooterBox>
+  );
 }
 
 export default Footer;
 const FooterBox = styled.footer`
-  height: 100px;
+  height: 65px;
   width: 100%;
-  background-color: #eee;
-`
+  background-color: var(--gray1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid var(--lightGray);
+`;
+
+const ContentsWrap = styled.div`
+  width: 1170px;
+  display: flex;
+  justify-content: space-between;
+
+  .align {
+    width: 28%;
+    display: flex;
+    gap: 25px;
+
+    div {
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 150%;
+      color: var(--gray4);
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+
+          span {
+            display: block;
+      width: 1px;
+      height: 10px;
+      background-color: var(--gray3);
+    }
+    }
+
+
+  }
+`;
+
+const FooterTitle = styled.section`
+  @media (max-width: 700px) {
+    padding: 0;
+  }
+`;
